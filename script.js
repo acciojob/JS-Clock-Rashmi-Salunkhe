0d742b72bdb14f0466adcb34774e8eb88ed73f19
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-08-24 22:47:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function setClock() {
 
   // Calculate the degrees for each hand
   const secondsDegrees = ((seconds / 60) * 360) + 90;  // Add 90 degrees to start at 12 o'clock
-  const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+ const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
   const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
 
   // Select the clock hands
